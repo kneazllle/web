8f90fc961d08515629109874176305ed3cbbf6bc
Abin Jose <abinjos307official@gmail.com>
2024-11-11 12:45:42 +0530

    Hero section made reponsive. Need to work on the navbar. Make a doker instead ofnavbar

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -18,15 +18,15 @@ export default function Hero() {
                     </p>
                 </div>
             </div>
-            <div className="row col-12 ">
-                <div className="col-lg-2 col-sm-12 d-flex justify-content-center flex-column">
-                    <div className="hero-sub-div my-1 py-2 px-4"><h2>500<span>+</span></h2><h6>Happy Clients</h6></div>
-                    <div className="hero-sub-div my-1 py-2 px-4"><h2>500<span>+</span></h2><h6>Happy Clients</h6></div>
-                    <div className="hero-sub-div my-1 py-2 px-4"><h2>500<span>+</span></h2><h6>Happy Clients</h6></div> 
+            <div className="row col-12 mx-auto ">
+                <div className="col-lg-2 col-sm-12 d-flex justify-content-center flex-lg-column flex-sm-row mx  -sm-2 mx-lg-0">
+                    <div className="hero-sub-div my-1 py-2 px-4"><h2>5<span>+</span></h2><h6>Happy Clients</h6></div>
+                    <div className="hero-sub-div my-1 py-2 px-4"><h2>5<span>+</span></h2><h6>Happy Clients</h6></div>
+                    <div className="hero-sub-div my-1 py-2 px-4"><h2>5<span>+</span></h2><h6>Happy Clients</h6></div> 
                 </div>
-                <div className="col-lg-10 col-sm-12 d-flex justify-content-center align-items-center">
-                    <img src={group} className="group-img" />
-                    
+                <div className="col-lg-10 col-sm-12 d-flex justify-content-center align-items-center" style={{ position: "relative" }}>
+                    <img src={group} className="group-img" alt="Group" />
+                    {/* <div className="rotate-div"></div> */}
                 </div>
             </div>
         </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,8 +7,8 @@ export default function Navbar() {
   return (
     <div>
       <div className='navbar py-4 px-5 row'>
-        <div className="col-2 d-flex justify-content-center"><img src={logo} className='logo-img' /></div>
-        <div className="col-8 mx-auto">
+        <div className="col-lg-2 col-sm-4 img-sec"><img src={logo} className='logo-img' /></div>
+        <div className="col-lg-8 d-none mx-auto nav-sec">
           <ul className="nav-list">
             <a href="/" className='list-item-link'>
               <li className="list-item">Home</li>
@@ -19,10 +19,25 @@ export default function Navbar() {
             </a><a href="" className='list-item-link'>
               <li className="list-item">About Us</li>
             </a>
+            
           </ul>
         </div>
-        <div className="col-2 contact-btn">
+        <div className="col-lg-2 col-sm-3 contact-btn">
+          <svg
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  className='hamburger'
+>
+  <rect y="4" width="24" height="2" fill="currentColor" />
+  <rect y="11" width="24" height="2" fill="currentColor" />
+  <rect y="18" width="24" height="2" fill="currentColor" />
+</svg>
           <a href="/contact" className='btn-contact px-3 py-2'>Contact Us</a>
+          
+
         </div>
       </div>
     </div>
