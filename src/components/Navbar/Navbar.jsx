7b3f1df35e1f154f71b14e7c297c10e8d6e0bd8d
Abin Jose <abinjos307official@gmail.com>
2024-11-15 16:27:43 +0530

    first commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,8 +7,8 @@ export default function Navbar() {
   return (
     <div>
       <div className='navbar py-4 px-5 row'>
-        <div className="col-lg-2 col-sm-4 img-sec"><img src={logo} className='logo-img' /></div>
-        <div className="col-lg-8 d-none mx-auto nav-sec">
+        <div className="col-lg-2 col-md-3 col-sm-4 img-sec"><img src={logo} className='logo-img' /></div>
+        <div className="col-lg-8 col-md-7 mx-auto nav-sec">
           <ul className="nav-list">
             <a href="/" className='list-item-link'>
               <li className="list-item">Home</li>
@@ -23,21 +23,7 @@ export default function Navbar() {
           </ul>
         </div>
         <div className="col-lg-2 col-sm-3 contact-btn">
-          <svg
-  width="24"
-  height="24"
-  viewBox="0 0 24 24"
-  fill="none"
-  xmlns="http://www.w3.org/2000/svg"
-  className='hamburger'
->
-  <rect y="4" width="24" height="2" fill="currentColor" />
-  <rect y="11" width="24" height="2" fill="currentColor" />
-  <rect y="18" width="24" height="2" fill="currentColor" />
-</svg>
           <a href="/contact" className='btn-contact px-3 py-2'>Contact Us</a>
-          
-
         </div>
       </div>
     </div>
